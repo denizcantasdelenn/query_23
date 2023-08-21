fa49bc33f6be53c77709ac3ea82eedfd85b9d925
denizcantasdelenn <136730167+denizcantasdelenn@users.noreply.github.com>
2023-08-21 12:00:33 +0300

    Add files via upload

---
+++ query_23.sql
@@ -0,0 +1,34 @@
+--find the cities where the covid cases are increasing continuously.
+
+create table covid(city varchar(50),days date,cases int);
+delete from covid;
+insert into covid values('DELHI','2022-01-01',100);
+insert into covid values('DELHI','2022-01-02',200);
+insert into covid values('DELHI','2022-01-03',300);
+
+insert into covid values('MUMBAI','2022-01-01',100);
+insert into covid values('MUMBAI','2022-01-02',100);
+insert into covid values('MUMBAI','2022-01-03',300);
+
+insert into covid values('CHENNAI','2022-01-01',100);
+insert into covid values('CHENNAI','2022-01-02',200);
+insert into covid values('CHENNAI','2022-01-03',150);
+
+insert into covid values('BANGALORE','2022-01-01',100);
+insert into covid values('BANGALORE','2022-01-02',300);
+insert into covid values('BANGALORE','2022-01-03',200);
+insert into covid values('BANGALORE','2022-01-04',400);
+
+
+--select * from covid
+
+
+with cte as (
+select *, 
+dense_rank() over(partition by city order by cases) - dense_rank() over(partition by city order by days) as differences
+from covid)
+
+select city
+from cte
+group by city
+having count(distinct differences) = 1 and avg(differences) = 0
